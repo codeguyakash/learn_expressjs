83ff152c8e0e9a62e64fadccce163e43ca61fd3c
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-04-15 16:09:19 +0530

    worker thread

--- index.js
+++ index.js
@@ -8,19 +8,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+//Problem is here
 app.get("/non-blocking/", (req, res) => {
-  res.status(200).send("This page is non-blocking");
+  res.status(200).send(`<h1>This page is non-blocking</h1>`);
+});
+app.get("/blocking", (req, res) => {
+  let count = 0;
+  for (let i = 0; i < 20_000_000_000; i++) {
+    count++;
+  }
+  console.log(count);
+  res.status(200).send(`<h1>This page is blocking ${count}</h1>`);
 });
 
-app.get("/blocking", (req, res) => {
-  const worker = new Worker("./worker.js");
-  worker.on("message", (data) => {
-    res.status(200).send(`Result is ${data}`);
-  });
-  worker.on("error", (error) => {
-    res.status(200).send(`Result is ${error}`);
-  });
-});
+//Solution is here
+
+// app.get("/blocking2", (req, res) => {
+//   const worker = new Worker("./worker.js");
+//   worker.on("message", (data) => {
+//     res.status(200).send(`Result is ${data}`);
+//   });
+//   worker.on("error", (error) => {
+//     res.status(200).send(`Result is ${error}`);
+//   });
+// });
 const userRouter = require("./routes/user.routes");
 app.use("/api/v1", userRouter);
 
